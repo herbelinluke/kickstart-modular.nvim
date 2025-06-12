b894ed9773440c73ff08285492eee1afa3564981
Luke Herbelin <herbelinluke@gmail.com>
2025-06-11 23:14:38 -0700

    some keymaps, harpoon, golf

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,10 @@
+-- custom keymaps
+-- PrimeAgen
 vim.keymap.set('n', '<leader>pv', '<cmd>Ex<CR>')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
